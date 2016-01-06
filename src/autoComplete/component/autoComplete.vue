d30c413958c5f1dc0e7142f57c81b27d5d01cab2
luyi <luyi@qipeipu.com>
2016-01-07 01:04:45 +0800

    修改package.json减少依赖

--- src/autoComplete/component/autoComplete.vue
+++ src/autoComplete/component/autoComplete.vue
@@ -1,21 +1,24 @@
-<link href="./autoComplete.css" type="text/css" rel="stylesheet">
 <template>
-	<ul class="auto-complete-list">
-		<li v-for="item in dataList" class="valid-item" v-on:click="chooseThisItem($index)">
-			<span v-if="item.leftText" class="left-text">{{item.leftText}}</span>
-			<span v-if="item.rightText" class="right-text">{{item.rightText}}</span>
-			<span v-if="item.history">{{item.history}}</span>
-			<span v-if="item.history" class="operation">
-				<em>历史记录</em><a href="javascript:void(0);" v-on:click="deleteHistory($index)">删除</a>
-			</span>
-			<input type="hidden" class="value" v-model="item.value" />
-		</li>
-		<li v-if="dataList.length === 0 && defaultText">
-			<span class="default-text">{{defaultText}}</span>
-		</li>
-	</ul>
+	<div class="auto-complete-container">
+		<input type="text" class="input" /><slot name="button"></slot>
+		<ul class="auto-complete-list">
+			<li v-for="item in dataList" class="valid-item" v-on="click:chooseThisItem($index)">
+				<span v-if="item.leftText" class="left-text">{{item.leftText}}</span>
+				<span v-if="item.rightText" class="right-text">{{item.rightText}}</span>
+				<span v-if="item.history">{{item.history}}</span>
+				<span v-if="item.history" class="operation">
+					<em>历史记录</em><a href="javascript:void(0);" v-on="click:deleteHistory($index)">删除</a>
+				</span>
+				<input type="hidden" class="value" v-model="item.value" />
+			</li>
+			<li v-if="dataList.length === 0 && defaultText">
+				<span class="default-text">{{defaultText}}</span>
+			</li>
+		</ul>
+	</div>
 </template>
 <script>
+	require('./autoComplete.css');
 	module.exports={
 		data:function(){
 			return{
--- src/autoComplete/index.js
+++ src/autoComplete/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by luyi on 2016/1/2.
  */
-var Vue = require('vue');
+/*var Vue = require('vue');*/
 var autoComplete = require('./component/autoComplete.vue');
 
 module.exports = new Vue({
